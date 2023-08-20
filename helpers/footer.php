558c72f187f8a25c65cfb32cac3c16d91429c06a
demeesterp <demeesterp@outlook.com>
2023-08-20 13:37:30 +0200

    change name

--- helpers/footer.php
+++ helpers/footer.php
@@ -1,4 +1,4 @@
 <?php function makefooter() {
-    echo "<hr/> <p class='text-end'><small>Copyright&copy; 2023 Piet Demeester</small></p>";
+    echo "<hr/> <p class='text-end'><small>Copyright&copy; 2023 Demeester Piet</small></p>";
 }
 ?>
