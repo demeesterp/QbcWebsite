1f2c5debbf7778f07ad1ed1b6bc5065db71bf13b
demeesterp <demeesterp@outlook.com>
2023-08-20 12:39:07 +0200

    add header tags

--- helpers/head.php
+++ helpers/head.php
@@ -3,6 +3,9 @@
 function makehead() {
     echo <<<'DELIMITER'
         <meta charset="utf-8" />
+        <meta name="author" content="Piet Demeester">
+        <meta name="description" content="The quantum theory applied to biology">
+        <meta name="keywords" content="quantum chemistry, biology, biochemistry, enzymes, molecular biology, lewis, acids, bases">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">   
         <link rel="stylesheet" href="../css/application.css">
